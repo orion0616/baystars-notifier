30d858cab2bb162e3c20ae3b953e062f86b8ebb8
orion46 <earth.nobu.light@gmail.com>
2019-03-02 20:58:23 +0900

    Adapt to new type of baystars web site
    Trimming title may has bug.

--- notifier.py
+++ notifier.py
@@ -5,62 +5,72 @@ import json
 import sys
 import datetime
 import logging
+from concurrent import futures
 from exlist import ExList
 
-logger = logging.getLogger(__name__)
-
 class News:
-    root = "http://www.baystars.co.jp"
-    send_list = []
+    def __init__(self, title, url):
+        self.title = title
+        self.url = url
+
+    def __str__(self):
+        return self.title + "\n(" + self.url + ")"
+
+# return today's date like this
+# "/2019/03/0301"
+def createTodayDate():
     now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
     year = str(now.year)
     month = str(now.month) if now.month >= 10 else '0' + str(now.month)
     day = str(now.day) if now.day >=10 else '0' + str(now.day)
+    return '/' + year + '/' + month + '/' + month + day
 
-    def __str__(self):
-        return self.title + "\n(" + News.root + self.path + ")"
+# return URL like this
+# "http://www.baystars.co.jp/news/2019/03/0301_01.php"
+def createURL(num):
+    root = 'http://www.baystars.co.jp/news'
+    suffix = '.php'
+    today = createTodayDate()
+    index = '0' + str(num+1)
+    url = root + today + '_' + index + suffix
+    return url
 
-    def __init__(self, link):
-        self.path = link.get("href")
-        self.title = link.string
-        [news_year, news_month, news_day] = self.path.split('/')[2:]
-        news_day = news_day[2:4]
-        if news_year == News.year and news_month == News.month and news_day == News.day:
-            News.send_list.append(self)
-
-def main():
-    logging.basicConfig()
-    logging.getLogger(__name__).setLevel(level=logging.DEBUG)
+# return News or None
+def crawl(num):
+    url = createURL(num)
     try:
-        r = requests.get(News.root, timeout=3)
+        r = requests.get(url, timeout=3)
     except requests.exceptions.Timeout:
-        logger.debug("Failed to fetch html!!")
-        sys.exit(1)
-    logger.info("Succeded to fetch html!!")
-
+        return None
     soup = bs4.BeautifulSoup(r.text, "html.parser")
-    news = ExList(soup.select("ul.newsList")[0].select("li"))
+    title = soup.find('title').text[:-15]
+    if title.find("見つかりません") != -1:
+        return None
+    news = News(title, url)
+    return news
 
-    news.foreach(lambda n: News(n.select("a")[0]))
+def makeText(newslist):
+    texts = [news.__str__() for news in newslist if news is not None]
+    if len(texts) == 0:
+        return "特にお知らせはありません\n"
+    return '\n\n'.join(texts)
 
-    if News.send_list:
-        message = "\n\n".join(ExList(News.send_list).map(lambda news: news.__str__()))
-    else:
-        message = "特にお知らせはありません\n"
-    logger.info("Message: " + message)
-
-    url = os.getenv("BAYSTARS_URL","")
-
+def sendSlackMessage(message):
+    webhookurl = os.getenv("BAYSTARS_URL","")
     try:
-        requests.post(url, data = json.dumps({
+        requests.post(webhookurl, data = json.dumps({
         'text': message, # 投稿するテキスト
         'username': u'baystars-news', # 投稿のユーザー名
         'link_names': 1, # メンションを有効にする
         }))
     except requests.exceptions.MissingSchema:
-        logger.debug("You have not set URL...")
         sys.exit(1)
-    logger.info("You have sent message to Slack!")
+
+def main():
+    with futures.ThreadPoolExecutor(max_workers=10, thread_name_prefix="thread") as executor:
+        newslist = list(executor.map(crawl, range(10)))
+    text = makeText(newslist)
+    sendSlackMessage(text)
 
 def exe(event, context):
     main()
